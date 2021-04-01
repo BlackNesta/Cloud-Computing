da90865ec8853df675b30e1069eec70d1efa9bfe
Andrei Manaila <andreimanaila@Andreis-MacBook-Pro.local>
2021-04-01 22:37:02 +0300

    street vie page

--- geoguesser/routes/index.js
+++ geoguesser/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const API_KEY = "AIzaSyBFvj2Pr18efj-xVFacYXz4-0M5OyqbWvY"
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Geoguesser' , api_key: API_KEY});
 });
 
 module.exports = router;
