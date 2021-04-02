0386dfb85981ddfd15d5be9da5d07b9bcc807add
drgvszi <33577111+drgvszi@users.noreply.github.com>
2021-04-02 16:49:01 +0300

    --

--- geoguesser/routes/index.js
+++ geoguesser/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 const API_KEY = "AIzaSyBFvj2Pr18efj-xVFacYXz4-0M5OyqbWvY"
+const LAT = "46.6407, 27.7276"
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Geoguesser' , api_key: API_KEY});
+  res.render('index', { title: 'Geoguesser' ,lat:LAT, api_key: API_KEY});
 });
 
 module.exports = router;
